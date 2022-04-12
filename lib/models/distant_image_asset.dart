1f8b9db43104be07e8b6bac9ea4adf48a2e5ed08
Said-Aabilla <said160aabilla@gmail.com>
2022-04-12 18:47:57 +0000

    added image asset list

---
+++ lib/models/distant_image_asset.dart
@@ -0,0 +1,13 @@
+class DsitantImageAsset {
+  final String id;
+  final String imageName;
+  final String imageLink;
+  final String modelLink;
+
+  DsitantImageAsset({
+    this.id,
+    this.imageLink,
+    this.imageName,
+    this.modelLink,
+  });
+}
--- lib/screens/multiple_augmented_images.dart
+++ lib/screens/multiple_augmented_images.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:arcore_flutter_plugin_example/models/distant_image_asset.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
@@ -16,6 +17,23 @@ class _MultipleAugmentedImagesPageState
   ArCoreController arCoreController;
   Map<String, ArCoreAugmentedImage> augmentedImagesMap = Map();
   Map<String, Uint8List> bytesMap = Map();
+
+  List<DsitantImageAsset> distantImages = [
+    DsitantImageAsset(
+        id: "1",
+        imageLink:
+            "https://github.com/Ahmed2000Github/spring-react/blob/modele/earth_augmented_image.jpg?raw=true",
+        imageName: "earth_augmented_image",
+        modelLink:
+            "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf"),
+    DsitantImageAsset(
+        id: "2",
+        imageLink:
+            "https://github.com/Ahmed2000Github/spring-react/blob/modele/earth_augmented_image.jpg?raw=true",
+        imageName: "prova_texture",
+        modelLink:
+            "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf"),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -39,14 +57,20 @@ class _MultipleAugmentedImagesPageState
   }
 
   loadMultipleImage() async {
-    final ByteData bytes1 = await NetworkAssetBundle(Uri.parse(
-            'https://github.com/Ahmed2000Github/spring-react/blob/modele/earth_augmented_image.jpg?raw=true'))
-        .load("");
-    final ByteData bytes2 = await rootBundle.load('assets/prova_texture.png');
-    final ByteData bytes3 = await rootBundle.load('assets/umano_digitale.png');
-    bytesMap["earth_augmented_image"] = bytes1.buffer.asUint8List();
-    bytesMap["prova_texture"] = bytes2.buffer.asUint8List();
-    bytesMap["umano_digitale"] = bytes3.buffer.asUint8List();
+    for (var asset in distantImages) {
+      final ByteData bytes =
+          await NetworkAssetBundle(Uri.parse(asset.imageLink)).load("");
+      bytesMap[asset.imageName] = bytes.buffer.asUint8List();
+    }
+
+    // final ByteData bytes1 = await NetworkAssetBundle(Uri.parse(
+    //         'https://github.com/Ahmed2000Github/spring-react/blob/modele/earth_augmented_image.jpg?raw=true'))
+    //     .load("");
+    // final ByteData bytes2 = await rootBundle.load('assets/prova_texture.png');
+    // final ByteData bytes3 = await rootBundle.load('assets/umano_digitale.png');
+    // bytesMap["earth_augmented_image"] = bytes1.buffer.asUint8List();
+    // bytesMap["prova_texture"] = bytes2.buffer.asUint8List();
+    // bytesMap["umano_digitale"] = bytes3.buffer.asUint8List();
 
     arCoreController.loadMultipleAugmentedImage(bytesMap: bytesMap);
   }
@@ -54,75 +78,46 @@ class _MultipleAugmentedImagesPageState
   _handleOnTrackingImage(ArCoreAugmentedImage augmentedImage) {
     if (!augmentedImagesMap.containsKey(augmentedImage.name)) {
       augmentedImagesMap[augmentedImage.name] = augmentedImage;
-      switch (augmentedImage.name) {
-        case "earth_augmented_image":
-          _addSphere(augmentedImage);
-          break;
-        case "prova_texture":
-          _addCube(augmentedImage);
-          break;
-        case "umano_digitale":
-          _addCylindre(augmentedImage);
-          break;
+
+      _addModel(augmentedImage);
+    }
+  }
+
+  Future _addModel(ArCoreAugmentedImage augmentedImage) async {
+    for (var asset in distantImages) {
+      if (asset.imageName == augmentedImage.name) {
+        final node = ArCoreReferenceNode(
+            objectUrl:
+                'https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf');
+
+        arCoreController.addArCoreNodeToAugmentedImage(
+            node, augmentedImage.index);
       }
     }
   }
 
-  Future _addSphere(ArCoreAugmentedImage augmentedImage) async {
-    // final ByteData textureBytes = await rootBundle.load('assets/earth.jpg');
+  // Future _addSphere(ArCoreAugmentedImage augmentedImage) async {
+  // final ByteData textureBytes = await rootBundle.load('assets/earth.jpg');
 
-    // final material = ArCoreMaterial(
-    //   color: Color.fromARGB(120, 66, 134, 244),
-    //   textureBytes: textureBytes.buffer.asUint8List(),
-    // );
-    // final sphere = ArCoreSphere(
-    //   materials: [material],
-    //   radius: augmentedImage.extentX / 2,
-    // );
-    // final node = ArCoreNode(
-    //   shape: sphere,
-    // );
+  // final material = ArCoreMaterial(
+  //   color: Color.fromARGB(120, 66, 134, 244),
+  //   textureBytes: textureBytes.buffer.asUint8List(),
+  // );
+  // final sphere = ArCoreSphere(
+  //   materials: [material],
+  //   radius: augmentedImage.extentX / 2,
+  // );
+  // final node = ArCoreNode(
+  //   shape: sphere,
+  // );
 
-    //  "https://github.com/Ahmed2000Github/spring-react/blob/modele/uploads_files_2894278_Robot.glb",
-    final node = ArCoreReferenceNode(
-        objectUrl:
-            'https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf');
+  //  "https://github.com/Ahmed2000Github/spring-react/blob/modele/uploads_files_2894278_Robot.glb",
+  //   final node = ArCoreReferenceNode(
+  //       objectUrl:
+  //           'https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf');
 
-    arCoreController.addArCoreNodeToAugmentedImage(node, augmentedImage.index);
-  }
-
-  void _addCube(ArCoreAugmentedImage augmentedImage) {
-    double size = augmentedImage.extentX / 2;
-    final material = ArCoreMaterial(
-      color: Color.fromARGB(120, 66, 134, 244),
-      metallic: 1.0,
-    );
-    final cube = ArCoreCube(
-      materials: [material],
-      size: vector.Vector3(size, size, size),
-    );
-
-    final node = ArCoreNode(
-      shape: cube,
-    );
-    arCoreController.addArCoreNodeToAugmentedImage(node, augmentedImage.index);
-  }
-
-  void _addCylindre(ArCoreAugmentedImage augmentedImage) {
-    final material = ArCoreMaterial(
-      color: Colors.red,
-      reflectance: 1.0,
-    );
-    final cylindre = ArCoreCylinder(
-      materials: [material],
-      radius: augmentedImage.extentX / 2,
-      height: augmentedImage.extentX / 3,
-    );
-    final node = ArCoreNode(
-      shape: cylindre,
-    );
-    arCoreController.addArCoreNodeToAugmentedImage(node, augmentedImage.index);
-  }
+  //   arCoreController.addArCoreNodeToAugmentedImage(node, augmentedImage.index);
+  // }
 
   @override
   void dispose() {
